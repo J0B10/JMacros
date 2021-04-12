fd0d19d1f373e70426f345ffbae84074533155cc
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-04-12 22:43:59 +0200

    fixed missing import

--- core/src/main/java/io/github/joblo2213/JMacros/core/config/ConfigManager.java
+++ core/src/main/java/io/github/joblo2213/JMacros/core/config/ConfigManager.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
